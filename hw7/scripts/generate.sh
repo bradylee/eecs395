15436cefb6fcf4060f3af4b5dad8bd550c79292c
Brady Lee <bradylee2016@u.northwestern.edu>
2015-12-10 12:30:09 -0600

    up to date

--- hw7/scripts/generate.sh
+++ hw7/scripts/generate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $1 -eq 0 ] 
+if [[ $1 -eq 0 ]] 
 then
     echo "Enter number of samples"
-else
+    exit 1
+fi
+    ./fm_radio test/usrp.dat
     head -c $(($1*4)) test/usrp.dat > sim/din.dat
     head -c $(($1/8*4)) test/usrp.out > sim/cmp.dat
-fi
